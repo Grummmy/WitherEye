fea16bb9cee1f2deef5b57020b59d5bc7219885e
Grumm <grummTheDev@gmail.com>
2025-03-19 22:06:27 -0400

    Update WitherEye.py

--- WitherEye.py
+++ WitherEye.py
@@ -1,4 +1,4 @@
-import os, subprocess
+import os, subprocess, requests, re, json
 from platform import uname
 from rich import print
 
@@ -11,21 +11,31 @@ from rich import print
 logo = "WitherEye"
 platform = uname()
 os.original_walk = os.walk
+url = "https://raw.githubusercontent.com/Grummmy/WitherEye/refs/heads/main/data.json"
+data = requests.get(url)
+if data.status_code == 200:
+    data = json.loads(data.text)
+else:
+    print(f"[red]Failed to load data from GitHub. [bold]Error {data.status_code}[/bold].[/red]")
+    print(f"[red]GitHub link:[/red] {url}")
+    exit()
+
+mc_launchers = re.compile(r"\b(" + "|".join(data["minecraft-launchers"]) + r")\b", re.IGNORECASE)
+
 
 def walk(path=".", n:int=-1):
     """
-    Walks directories like os.walk(), but limits depth with 'n'.
+    Walks directories like os.walk(), but limits depth with `n`.
     
     Args:
         path (str): Starting path (default: ".").
-        n (int): Max depth (-1 for unlimited).
-    
+        n (int): Max depth (-1 for unlimited, 0 to walk through `path` only).
     Yields:
         tuple: (root, dirs, files) for each directory.
     """
     path = os.path.abspath(path)
     for root, dirs, files in os.original_walk(path):
-        if root[len(path):].count(os.sep) > n:
+        if n != -1 and root[len(path):].count(os.sep) > n:
             return
         yield root, dirs, files
 
@@ -48,31 +58,22 @@ def find_minecraft():
     Finds Minecraft, hopefully, on all systems!
     if it's present at all.
     """
-    if platform.system[0] == "W":
-        path = {
-            "m": os.path.expandvars("%appdata%\\.minecraft"),
-            "t": os.path.expandvars("%appdata%\\.tlauncher")
-        }
-        minecraft = ""
+    appdata = os.environ.get("APPDATA", os.path.expanduser("~"))
+    minecraft = []
+    for root, dirs, files in os.walk(appdata, 1):
+        for dir in dirs:
+            if mc_launchers.search(dir):
+                minecraft.append((os.path.join(root, dir), root + os.sep + mc_launchers.sub(r"\033[1m\1\033[0m", dir)))
+    
+    while not minecraft or not os.path.exists(minecraft[0][0]):
+        try:
+            minecraft = input("Minecraft was not found. Please enter the path, or exit(Ctrl+C) the program.\n").strip()
+            
+        except KeyboardInterrupt:
+            print("Have a good day, sir! And do not cheat!")
+            exit()
 
-        if os.path.exists(path["m"]):
-            minecraft = os.path.abspath(path["m"])
-        elif os.path.exists(path["t"]):
-            # if there are folders "Legacy" and "launcher", to ensure that this is Tl Legacy, and not Tlauncher.
-            dirs = {entry.name for entry in os.scandir(path["t"]) if entry.is_dir()}
-            if {"legacy", "logs"}.issubset(dirs):
-                minecraft = os.path.abspath(path["t"])
+# if match := re.search(r"java", os.environ["PATH"], re.I):
+#     print(match.group())
 
-        while (not os.path.exists(minecraft)) or (not minecraft):
-            proccess = subprocess.run('powershell -command "& {Add-Type -AssemblyName System.windows.forms; $f=New-Object System.Windows.Forms.FolderBrowserDialog; $f.ShowDialog(); $f.SelectedPath}"', capture_output=True, text=True)
-            if proccess.stdout[:2] == "OK":
-                minecraft = proccess.stdout.strip().split("\n")[1]
-            else:
-                minecraft = input("Where is your main Minecraft folder?\n")
-        
-        return minecraft
-
-    elif platform.system[0] == "L":
-        pass
-
-print(find_minecraft())
+print("\n".join(path[0] for path in find_minecraft()))
